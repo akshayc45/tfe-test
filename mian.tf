fe5f0b9802b8cf4bd43241954f5eb325b2ac4bb9
akshayc45 <84924892+akshayc45@users.noreply.github.com>
2024-03-01 15:25:14 +0530

    Update mian.tf

--- mian.tf
+++ mian.tf
@@ -1,5 +1,15 @@
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo 'This is another echo command.'"
+
+    
+      terraform {
+  required_providers {
+    openstack = {
+      source = "app.terraform.io/test-26022024/openstack"
+      version = "1.42.0"
+    }
   }
 }
+
+provider "openstack" { 
+  # Configuration options 
+}
+    
